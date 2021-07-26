8e91932a0ad9a82dcec8f3b7e9a2f41125a8a3ae
Vahid Sayad <vahidsayad@gmail.com>
2021-07-26 20:17:27 +0430

    Initial Commit

---
+++ Weather in SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather in SwiftUI
+//
+//  Created by Vahid Sayad on 7/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather in SwiftUI/Weather_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_in_SwiftUIApp.swift
+//  Weather in SwiftUI
+//
+//  Created by Vahid Sayad on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct Weather_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
